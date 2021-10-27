4fe87878d80953928e19ec34a61a0ef4f4faaae1
Thomas CHERRIER <75333938+thomascherrier@users.noreply.github.com>
2021-10-27 16:00:47 +0200

    commit3

---
+++ api/tasks-get/index.js
@@ -0,0 +1,23 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const tasks = [
+        {
+            id: 1,
+            label: "🍔 Eat",
+            status: ""
+        },
+        {
+            id: 2,
+            label: "🛏 Sleep",
+            status: ""
+        },
+        {
+            id: 3,
+            label: "</> Code",
+            status: ""
+        }
+    ];
+
+}
+
